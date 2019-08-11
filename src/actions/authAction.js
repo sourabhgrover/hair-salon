740f1a9aee18fde18389f3feb4d380ecf1a532ab
sourabh <sourabh.grover@trantorinc.com>
2019-08-11 21:46:11 +0530

    Upated Axios Configuration after successfull login

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -7,17 +7,17 @@ export const login = data => {
     const response = await hairSalonApi.post("/users/login", data);
     const token = response.data.response.token;
     localStorage.setItem("jwtToken", token);
+    hairSalonApi.defaults.headers.common["Authorization"] = token;
     dispatch({
       type: SIGN_IN,
       payload: response.data.response
     });
-    hairSalonApi.defaults.headers.common["Authorization"] =
-      localStorage.jwtToken;
     history.push("/allUserList");
   };
 };
 
 export const logout = () => {
   localStorage.removeItem("jwtToken");
+  history.push("/");
   return { type: SIGN_OUT };
 };
